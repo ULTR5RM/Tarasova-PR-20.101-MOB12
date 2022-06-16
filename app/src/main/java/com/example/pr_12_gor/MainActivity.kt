88bc3e789e8a2cb7c0230d8081b779219ca0ec6e
ULTR5RM <ULTR.RM79@gmail.com>
2022-06-17 01:54:01 +0700

    Tarasova PR-20.101

---
+++ app/src/main/java/com/example/pr_12_gor/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.pr_12_gor
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pr_12_gor.ui.theme.PR_12_GorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PR_12_GorTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PR_12_GorTheme {
+        Greeting("Android")
+    }
+}
